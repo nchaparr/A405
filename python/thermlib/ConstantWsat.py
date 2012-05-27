94e752b54bdc7ce505ff66c38c8c82496b76f4d7
Niamh Chaparro <nchaparr@owl.eos.ubc.ca>
2012-05-26 18:28:45 -0700

    corrected version for wsat = 0.01 kg/kg

--- python/thermlib/ConstantWsat.py
+++ python/thermlib/ConstantWsat.py
@@ -8,13 +8,16 @@ import matplotlib.pyplot as plt
 """This script finds the Temperature which returns zero for the
    following function:
    Wsat0Temp = Wsat - Wsat0
-   where Wsat = 0.644*Esat/(press - Esat)
-   and Esat = 6.112*exp(17.67*Temp/(Temp +243.3))
+   where Wsat = 0.622*Esat/(press - Esat)
+   and Esat = 611.2*exp(17.67*Temp(C)/(Temp(C) +243.3))
+
+   Inputs are press in Pa and Temp in K (converted to C)
+
 """
 
 def WsatShift(Temp, press, Wsat0):
-    Esat = 6.11*np.exp(17.67*1.0*(Temp - 273.15)/((Temp - 273.15) + 243.5))
-    Wsat = 0.644*(1.0*Esat/(press -Esat))
+    Esat = 611.2*np.exp(17.67*1.0*(Temp - 273.15)/((Temp - 273.15) + 243.5))
+    Wsat = 0.622*(1.0*Esat/(press -Esat))
     if Wsat > 0.060:
         Wsat = 0.060
     elif Wsat < 0:
@@ -23,12 +26,12 @@ def WsatShift(Temp, press, Wsat0):
     return WsatShift
 
 def WsatTemp(press, Wsat0):
-    WsatTemp = optimize.zeros.brenth(WsatShift, 200, 400, (press, Wsat0))
+    WsatTemp = optimize.zeros.brenth(WsatShift, 200, 330, (press, Wsat0))
     return WsatTemp
 
 if __name__=="__main__":
-    
+
     Wsat0 = .01
     p = 80000 
-   
+    print WsatShift(200, p, Wsat0), WsatShift(330, p, Wsat0)
     print WsatTemp(p, Wsat0)
