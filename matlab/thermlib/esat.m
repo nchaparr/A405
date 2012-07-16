8f90da2eec64a82a2e8f0977c44aa2fffc212dc0
Niamh Chaparro <nchaparr@owl.eos.ubc.ca>
2012-07-16 10:56:42 -0700

    miscellaneous

--- matlab/thermlib/esat.m
+++ matlab/thermlib/esat.m
@@ -1,4 +1,4 @@
-function [esatout]=esat(T)
+function [esatout]=esat(T);
   % in:         T = temperature in Kelvin (vector),
   % out:     esat = saturation water vapour pressure over water in Pa 
   % reference: Emanuel 4.4.14 p. 117
