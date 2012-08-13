d3990f3f2c393e22a645e8fa3117ef1f90f609ba
Niamh Chaparro <nchaparr@owl.eos.ubc.ca>
2012-08-13 14:03:51 -0700

    nothing

--- python/thermlib/constants.py
+++ python/thermlib/constants.py
@@ -5,7 +5,7 @@ class constants:
    References
    - - - - - -
    Emanuel appendix 2
-   
+
    """
    Tc = 273.15; #0 deg C in Kelvin
    eps = 0.622;
@@ -14,6 +14,7 @@ class constants:
    lv0 = 2.501e6; # J/kg
    Rv = 461.50; # J/kg/K
    Rd = 287.04; # J/kg/K
+   
    cpv = 1870.; # Heat capacity of water vapor (J/kg/K)
    cl = 4190.;  # Heat capacity of liquid water (J/kg/K)
    cpd = 1005.7; # Heat capacity of dry air (J/kg/K)
