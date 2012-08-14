8440ee82815897f2312905812fcdba43fa45be60
Niamh Chaparro <nchaparr@owl.eos.ubc.ca>
2012-08-14 15:08:22 -0700

    calcVars: took out unecessary references to thetaeVal, other

--- python/ode45/calcVars.py
+++ python/ode45/calcVars.py
@@ -1,4 +1,3 @@
-
 import site
 site.addsitedir('/nfs/kite/home/nchaparr/A405/repos/a405repo/python/thermlib')
 from constants import constants as c
@@ -8,58 +7,50 @@ from findWvWl import findWvWl
 from esat import *
 from T_thetaep import t_thetaep
 
-def calcBuoy(height, Wt, Tparc, thetae0, interpTenv, interpTdEnv, interpPress):
+def calcBuoy(height, Wt, Tparc, interpTenv, interpTdEnv, interpPress):
 
     #input: height (m), thetae0 (K), plus function handles for
     #T,Td, press soundings
     #output: Bout = buoyant acceleration in m/s^2
     #neglect liquid water loading in the virtual temperature
     
-    Press=interpPress(height)*100 #Pa
-    #Tparc=t_thetaep(thetae0, Wt, Press) #K
-    wvparc= findWvWl(Tparc, Wt, Press)[0]; #kg/kg
-    Tvparc=Tparc*(1. + c.eps*wvparc)
-    Tenv=interpTenv(height) + c.Tc
-    Tdenv=interpTdEnv(height) + c.Tc
-    wvenv=wsat(Tdenv,Press); #kg/kg
-    Tvenv=Tenv*(1. + c.eps*wvenv)
-    TvDiff=Tvparc - Tvenv
+    Press = interpPress(height)*100 #Pa
+    [wvparc, wlparc] = findWvWl(Tparc, Wt, Press) #kg/kg
+    Tvparc = Tparc*(1. + c.eps*wvparc)
+    Tenv = interpTenv(height) + c.Tc
+    Tdenv = interpTdEnv(height) + c.Tc
+    wvenv = wsat(Tdenv,Press) #kg/kg
+    Tvenv = Tenv*(1. + c.eps*wvenv)
+    TvDiff = Tvparc - Tvenv
     dW = c.g0*(TvDiff/Tvenv)    
     return dW 
 
-def calcdT(height, Wt, Tparc, interpPress, thetae0, Wvel):
+def calcdT(height, Wt, Tparc, interpPress, Wvel):
     Press=interpPress(height)*100.
-    #Tparc=findTmoist(thetae0, Press)
-    wvparc=findWvWl(Tparc, Wt, Press)[0]
-    wl = Wt - wvparc
+    [wvparc, wlparc] = findWvWl(Tparc, Wt, Press)
     Tvparc=Tparc*(1. + c.eps*wvparc)
     rho = 1.0*Press/(c.Rd*Tvparc)
     Ws = wsat(Tparc, Press)#assumed saturated?
 
-    rho= 1.0*Press/(c.Rd*Tvparc) #density of the cloud
+    rho= 1.0*Press/(c.Rd*Tvparc) #density of the parcel
 
-    Pressv = 1.0*(Ws/(Ws + c.eps))*Press
+    Pressv = 1.0*(wvparc/(wvparc + c.eps))*Press
     
     dWs=(Ws + Ws**2)*(1.0*c.lv0/(c.Rv*Tparc**2))
+
+    Pressd = Press - Pressv
     
-    dT = (1.0*c.Rd*Tparc/(c.cpd*Press))*Wvel*(-c.g0*rho)*(1.0/(1 - (c.lv0*Ws)/(c.cpd*Tparc) + (1.0*c.lv0/c.cpd)*dWs))
+    dT = (1.0*c.Rd*Tparc/(c.cpd*Pressd))*Wvel*(-c.g0*rho)*(1.0/(1 - (c.lv0*Ws)/(c.cpd*Tparc) + (1.0*c.lv0/c.cpd)*dWs))
 
     a = 1.0*(1+Wt)/(1+wvparc*(1.0*c.cpv/c.cpd))
     b = 1+1.0*c.lv0/(c.Rd*Tparc)
-    C = 1.0*wl*c.cl/(c.cpd+wvparc*c.cpv)
+    C = 1.0*wlparc*c.cl/(c.cpd+wvparc*c.cpv)
     d = 1.0*c.lv0**2*wvparc*(1 + 1.0*wvparc/c.eps)/(c.Rv*Tparc**2*(c.cpd + wvparc*c.cpv))
 
     check1dT = -1.0*(1.0*c.g0*Wvel/c.cpd)*a*b/(1+C+d)
     checkdT = -1.0*(c.g0*Wvel)/(c.cpd + c.lv0*dWs)
-    #print Ws, dT
+    
     return dT, checkdT, check1dT
-
-def calcdWdT(tempK, pressPa):
-    deriv=c.lv0/(c.Rv*tempK**2.)  #1/es des/dT
-    thewsat=wsat(tempK,pressPa)
-    #chain rule the derivitive of epsilon*esat/(p - esat)
-    out= (thewsat[0]  + 1.0*thewsat[0]**2/c.eps)*deriv
-    return out
 
 def calcdr(rad, Tparc, height, interpPress, rho_a, r_a, M_a, I_no):
     rho_d = 1000 #can i assume this?
