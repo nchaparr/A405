d9cb7125577d509205bc23931fa1910b0079f78a
Niamh Chaparro <nchaparr@owl.eos.ubc.ca>
2012-09-12 09:06:29 -0700

    aerodist: changes to be included in model

--- python/ode45/aerodist.py
+++ python/ode45/aerodist.py
@@ -19,9 +19,9 @@ LogDist():
    between log(themass(i)) and log(themass(i+1))
 """
   
-def logDist(themass,Amp,meanval,logsd):
+def logDist(themass, Amp, meanval, logsd):
     theexp=np.exp(-0.5*(((np.log(themass) - np.log(meanval))/logsd))**2.);
-    out=Amp/(themass*np.sqrt(2*np.pi)*logsd)*theexp;
+    out=Amp/(np.sqrt(2*np.pi)*logsd)*theexp;
     print 'array of masses', themass
     print 'probabilities', out
     return out 
@@ -33,18 +33,18 @@ def Aero_dist():
     rhoaero = 1775
     Mtot =.25e-9 #0.25 mug/m^3
     logsd = 1.7
-    meandiam = 0.2e-6
+    meandiam = 0.2*10**-6
     meanmass = 1.0*rhoaero*4/3*np.pi*(meandiam/2.)**3.
 
 #mass bins from 0.01 to 0.5 microns
 #start with log10(mass)
-    logdrydiam = np.linspace(-2, 1, num = 50)
+    logdrydiam = np.linspace(-2, .1, num = 24)
 #convert to meters
     drydiam = 10.0**logdrydiam*1.0e-6
     dryrad = 1.0*drydiam/2
     
 #convert aerosol volume to kg
-    themass = rhoaero*(1.0*4/3)*np.pi*(drydiam/2.)**3.0
+    themass = rhoaero*(1.0*4/3)*np.pi*(dryrad)**3.0
 
 #get the number distribtuion for themass vector
     out = logDist(themass, Mtot, meanmass, logsd)
@@ -52,19 +52,19 @@ def Aero_dist():
 
 #now divide this by the bin mass, which gives
 #the number of aerosols in each bin
-    thenum = np.divide(out, themass)
+    thenum = np.divide(1.0*out, themass)
 
     end = len(themass)
+    print 'end', end
 # now work backwards to check our number distribution
-    dlogm = np.subtract(np.log(themass[1:end]), np.log(themass[0:end-1]))
-    dD = np.subtract(themass[1:end], themass[0:end-1])
+    dlogMass = np.subtract(np.log(themass[1:end]), np.log(themass[0:end-1]))
     print 'check total mass:'
-    binmass = np.array([x*y for x in out[1:end]for y in dD])
+    binmass = np.array([x*y for x in out[0:end-1]for y in dlogMass])
     totmass=np.sum(binmass)
     print totmass
     print 'check total number'
     end = len(thenum)
-    binnum = np.array([x**y for x in thenum[1:end] for y in dlogm])
+    binnum = np.array([x*y for x in thenum[1:end] for y in dlogMass])
     totnum = np.sum(binnum)
     print totnum
 
@@ -77,10 +77,11 @@ if __name__ == "__main__":
     fig=plt.figure(1)
     fig.clf()
     ax1 = fig.add_subplot(111)
-    plt.semilogx(themass,out)
-    plt.title('lognormal mass distribution')
-    plt.xlabel('aerosol mass (kg)')
-    plt.ylabel('m x mu(m) (kg/m^3/per log binwidth)')
+    #plt.semilogx(themass,out)
+    plt.plot(themass, out)
+    #plt.title('lognormal mass distribution')
+    plt.xlim(-10**-17, 10**-15)
+    #plt.ylabel('m x mu(m) (kg/m^3/per log binwidth)')
     plt.show()
 
    
