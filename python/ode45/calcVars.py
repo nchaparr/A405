6282b670e167315a0a2edd820bea9eecca7b73ec
Niamh Chaparro <nchaparr@owl.eos.ubc.ca>
2012-10-19 10:05:13 -0700

    working version with docstring

--- python/ode45/calcVars.py
+++ python/ode45/calcVars.py
@@ -8,7 +8,7 @@ from esat import *
 from T_thetaep import t_thetaep
 from nchap_ode_littlerock import drop_props
 """
-For use in 
+For use in nchap_ode_littlerock
 
 Input:
     height (m)
@@ -24,8 +24,11 @@ output:
     dW = buoyant acceleration in m/s^2
     dT = rate of change of temperature
     dr = rate of change of 
-    dSS = rate of change of 
+    dSS = rate of change of Super Saturation 
 
+Functions Defined:
+     aero_prms(sigma_w, I_no, M_a, M_w, rho_a, r_a, r, rho_w, TempK):
+     returns a and b parameters per W&H ch6 Ex 12
     """
 
 #def calc_Vars(height, Wt, Tparc, Wvel, rad, SS, rho_a, r_a, r0, M_a, I_no, interpTenv, interpTdEnv, interpPress):
@@ -62,78 +65,51 @@ def calc_Vars(height, Wt, r0, r_a, Num_a, Tparc, Wvel, rad, SS, rho_a, M_a, I_no
     sigma_w = .076
     M_w = 18*10**-3 #kg/mol
 
-    #Test on total water according to adiabats
-     
+    #for test on total water according to adiabats
     dWsdT=(Ws + Ws**2)*(1.0*c.lv0/(c.Rv*Tparc**2))
-    
     dWsdP = -1.0*(c.eps*es)/(Press - es)**2
-
     rhovs = es/(c.Rv*Tparc) 
     
     Dv = 100000.0*(2.21*10**-5)/Press #Diffusion constant, Curry&Webster p144 
     
-    #v_a = []
     dr = []
     dwl = []
     for i in range(len(rad)):
-        #v_a.append((1.0*4/3)*np.pi*r_a[i]**3)
         a, b = aero_prms(sigma_w, I_no, M_a, M_w, rho_a, r_a[i], rad[i], rho_w, Tparc)
-        #if rad[i] <= r0[i] and (1.0/rad[i])*(1.0*Dv*rhovs/rho_l)*(SS) < 0:
-        #   drad = 0  
-        #else:
-        #print 'difference in SS values', (SS - 1.0*a/rad[i] + 1.0*b/rad[i]**3)
         if ((SS - 1.0*a/rad[i] + 1.0*b/rad[i]**3) < 0 and rad[i] <= r0[i]):
-            #print "zero drad"
             drad = 0
         else:
              drad = (1.0/rad[i])*(1.0*Dv*rhovs/rho_l)*(SS - 1.0*a/rad[i] + 1.0*b/rad[i]**3)
-        #    drad = (1.0/rad[i])*(1.0*Dv*rhovs/rho_l)*(SS)
-        #print 'drad', drad, SS + 1, 1 + 1.0*a/rad[i] - 1.0*b/rad[i]**3     
         dr.append(drad)
         dwl.append(Num_a[i]*rho_l*4.0*np.pi*((rad[i])**2)*drad)
     cumdwl = np.sum(np.array(dwl))
     if drad>0:
         dwv = -cumdwl
     else:
-        dwv = 0             
-                    
+        dwv = 0                        
     des = 1.0*(c.lv0*es)/(c.Rv*(Tparc**2)) 
-   
 
-    if Wt < Ws:
+    if Wt < Ws: #alternate dT equations
         #dT = (-1.0*c.g0/(c.cpd))*(Wvel)
-        #dT = -(1.0*c.Rd*Tparc/(c.cpd*Pressd))*c.g0*rho*Wvel
+
+
         dT = -(1.0*c.Rd*Tparc/(c.cpd*Pressd))*(c.g0*rho*Wvel)
     else:   
         #dT = -(1.0*c.lv0)/(c.cpd)*dwv - (1.0*c.g0)/(c.cpd)*Wvel 
-        #dT = ((1.0*c.Rd*Tparc)/(c.cpd*Pressd) - (1.0*c.lv0/c.cpd)*dWsdP)/(1 - (c.lv0*Ws/(c.cpd*Tparc)) + (c.lv0*dWsdT)/(c.cpd))*Wvel*(-rho*c.g0)
-
         dT = (1.0*(c.Rd*Tparc)/(c.cpd*Pressd) - 1.0*(c.lv0*dWsdP)/(c.cpd))*Wvel*(-c.g0*rho)*(1.0/(1 - (c.lv0*Ws)/(c.cpd*Tparc) + (1.0*c.lv0/c.cpd)*dWsdT))
         
     term1 = (-c.eps*es/Press**2)*(-c.g0*Press*Wvel/(c.Rd*Tparc)) 
     term2 = (c.eps/Press)*(des)*dT
-    dwv = 0
     dSS =  (Press/(c.eps*es))*(dwv - (1+SS)*(term1 + term2))
-    #print''
-    #print'SS, dSS',SS, dSS
-    #print 'Wvel, dW', Wvel, dW
-    #print 'T, dT', Tparc, dT
-    #print 'r, dr', rad, dr
-    #print''
     return dW, dT, dSS, dr 
 
-def aero_prms(sigma_w, I_no, M_a, M_w, rho_a, r_a, r, rho_w, TempK):
-    
+def aero_prms(sigma_w, I_no, M_a, M_w, rho_a, r_a, r, rho_w, TempK): 
     v_d = 1.0*4/3*np.pi*(r**3)
     v_a = 1.0*4/3*np.pi*(r_a**3)
     m_a = rho_a*v_a
     rho_d = 1.0*(m_a + (v_d - v_a)*rho_w)/v_d
-        
     a = 1.0*2*sigma_w/(rho_w*c.Rv*TempK)
     b = (1.0*(I_no*rho_a*M_w)/(M_a*rho_d))*(r_a**3)    
-
-    #print 'test on a and b', a*TempK, 1.0*b/I_no*(1.0*M_a/m_a)
-
     return a, b
 
     
